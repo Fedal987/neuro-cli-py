ab5d6d1aae8817038f1d5e4e7ae4c17429325f2c
Fedal <105250639+Fedal987@users.noreply.github.com>
2026-04-29 13:31:47 +0800

    Add files via upload

---
+++ neuro.py
@@ -0,0 +1,11 @@
+"""
+    Neuro-cli
+    author@Fedal987
+    Powered by SigmaStudio
+    GitHub: https://github.com/Fedal987/neuro-cli
+"""
+
+from src.main.ui.terminal_cli import main
+
+if __name__ == "__main__":
+    main()
