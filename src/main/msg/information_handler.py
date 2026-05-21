860042729816901e3593c6ad535108e0a1894a30
Fedal <105250639+Fedal987@users.noreply.github.com>
2026-05-21 13:39:57 +0800

    feat: improved information_handler

--- src/main/msg/information_handler.py
+++ src/main/msg/information_handler.py
@@ -1,6 +1,29 @@
+"""
+    Neuro-cli
+    author@Fedal987
+    Powered by SigmaStudio
+    GitHub: https://github.com/Fedal987/neuro-cli-py
+"""
+
 import time
 import requests
 import socket
+import platform
+import psutil
+
+
+os = platform.system()
+cpu_core_count = psutil.cpu_count(logical=False)
+cpu_usage = psutil.cpu_percent(interval=1)
+memory = psutil.virtual_memory()
+total_mem = memory.total
+used_mem = memory.used
+avaliable_mem = memory.available
+disk = psutil.disk_usage('/')
+total_disk = disk.total
+used_disk = disk.used
+avaliable_disk = disk.free
+local_nw = psutil.net_if_addrs()
 
 def local_time():
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
